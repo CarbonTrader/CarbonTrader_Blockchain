7039a7d50a76a1ba49cfc4ae7ddd05cbc07587f5
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2022-08-22 19:41:43 -0500

    Thread handling

--- Node.py
+++ Node.py
@@ -1,9 +1,8 @@
 import socket as Socket
 import concurrent.futures
-from threading import current_thread
+from threading import current_thread, enumerate
 import time
 import struct
-import sys
 
 # The following constants are standard for the whole network.
 SRC_PORT = 5556
@@ -18,6 +17,8 @@ class Node:
     It functions both as server and client, simultaneously, and is used to perform validation of blocks
     given the [consensus] protocol.
     """
+
+    node_is_alive = True
 
     def __init__(self, machine_ip_address):
         self.ip_address = machine_ip_address
@@ -63,18 +64,19 @@ class Node:
     i.e. number of nodes in the network - 1.
     """""
     def heartbeat(self, dst_port, src_port):
-        
+
         print(f'Heartbeat thread: {current_thread().name}')
 
         sock = Socket.socket(Socket.AF_INET, Socket.SOCK_DGRAM)
         sock.bind(('0.0.0.0', src_port))
-        while True:
+        while self.node_is_alive:
             for peer_address in PEER_ADDRS:
                 if peer_address == self.ip_address:
                     continue
                 sock.sendto(f'{self.ip_address}'.encode(), (peer_address, dst_port))
                 print(f'Message sent to peer with address {peer_address}')
                 time.sleep(1)
+        return None
 
     """""
     The following function is similar to the one above. 
@@ -100,11 +102,10 @@ class Node:
     The remaining, sends a heartbeat message to each of this node's peers. 
     """""
     def execute(self):
-        node_is_alive = True
         with concurrent.futures.ThreadPoolExecutor() as thread_executor:
             try:
-                while node_is_alive:
-                    port_listener = thread_executor.submit(self.listen, DST_PORT)
+                while self.node_is_alive:
+                    port_listener_thread = thread_executor.submit(self.listen, DST_PORT)
                     # thread_executor.submit(self.listen_multicast, MCAST_ADDR_GROUP, DST_PORT)
                     
                     thread_executor.submit(self.heartbeat, DST_PORT, SRC_PORT)
@@ -112,8 +113,9 @@ class Node:
 
                     # 'timeout' parameter sets a timer which, when finishing the countdown, 
                     # if no data has been received, the thread raises a TimeoutError exception.
-                    port_listener.result(timeout = 10)
+                    port_listener_thread.result(timeout = 10)
             except concurrent.futures.TimeoutError:
-                node_is_alive = False
-                print('Node is dead. Please run again.')
+                self.node_is_alive = False
+                print('Node is dead. Stopping all tasks...')
                 thread_executor.shutdown(wait = False)
+                
