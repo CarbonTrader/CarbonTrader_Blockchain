86223030e0edd3bc8f46e1208195f6bda1b76867
monotera <nelson.mosquera@javeriana.edu.co>
2022-09-06 22:57:52 -0500

    FailSafe

--- main.py
+++ main.py
@@ -1,50 +1,77 @@
-from asyncore import read
 import json
+import os
 import random
 import threading
 from concurrent import futures
-from concurrent.futures import ThreadPoolExecutor
-
-import os
-from google.auth import jwt
+import time
 from google.cloud import pubsub_v1
-import json
+from pydantic import BaseSettings
 
 NODE_ID = 'Node1'
+MAX_TRANSACTIONS_PER_BLOCK = 3
+class Settings(BaseSettings):
+    CREDENTIALS_PATH: str
+    project_id = 'flash-ward-360216'
+    api_topic_subscription_id = 'vocero-sub-' + NODE_ID
+    node_topic_id = 'nodes_info'
+    api_topic_id = 'vocero'
 
-count = 1
+settings = Settings()
+
+# GCP Auth
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = settings.CREDENTIALS_PATH
+
+# Creando el suscriptor
+subscriber = pubsub_v1.SubscriberClient()
+api_topic_subscription_path = subscriber.subscription_path(
+    settings.project_id, settings.api_topic_subscription_id)
+
+# Se inicializa el publisher
+api_publisher = pubsub_v1.PublisherClient()
+api_topic_path = api_publisher.topic_path(settings.project_id, settings.api_topic_id)
 
 
 def handle_consensus_reception(sender, number):
-    global count
     print('Mensajero: {}, Numero: {}'.format(sender, number))
 
 
 def handle_api_message(message):
-    print('-----------------------------------------------------------------------------------------------------------')
-    global count
     idtransaction = message['idTransaction']
     transaction = message['transactions']
     print('id: {}'.format(idtransaction))
     print('transaction: {}'.format(transaction))
-    print(count)
-    if count == 3:
-        print("Llegaron 3 transacciones")
-        number = generate_random_number()
-        print('Genere:{}'.format(number))
-        count = 1
-        data = {
-            'type': 'node_message',
-            'sender': NODE_ID,
-            'number': number,
-        }
-        message_to_send = json.dumps(data, ensure_ascii=False).encode('utf8')
-        future1 = api_publisher.publish(api_topic_path, message_to_send)
-        future1.result()
+    data = read_json("local_transactions.json")
+    data.append(transaction)
+    write_json("local_transactions.json",data)
+    validate_number_transactions(data)
 
-    else:
-        count += 1
 
+
+def validate_number_transactions(data):
+    if len(data) >= MAX_TRANSACTIONS_PER_BLOCK:
+        consensus_thread = threading.Thread(target=consensus_algorithm())
+        consensus_thread.start()
+
+def consensus_algorithm():
+    number = generate_random_number()
+    print('Genere:{}'.format(number))
+    data = {
+        'type': 'node_message',
+        'sender': NODE_ID,
+        'number': number,
+    }
+    message_to_send = json.dumps(data, ensure_ascii=False).encode('utf8')
+    future1 = api_publisher.publish(api_topic_path, message_to_send)
+    future1.result()
+    # TODO: Death nodes
+    nodes = read_json("nodes.json")
+    while not is_nodes_done(nodes):
+        print("waiting")
+        time.sleep(5)
+    print("Recibí todos los mensajes")
+    print("Gano: {}".format(get_consensus_winner()))
+    reset_consensus_nodes()
+    write_json("local_transactions.json",[])
 
 def generate_random_number():
     number = random.uniform(0, 1)
@@ -62,7 +89,7 @@ def read_json(filename):
 
 def write_json(filename, data):
     with open(filename, 'w') as fp:
-        json.dump(data, fp,  sort_keys=False, indent=4, separators=(',', ': '))
+        json.dump(data, fp, sort_keys=False, indent=4, separators=(',', ': '))
 
 
 def get_consensus_winner():
@@ -94,13 +121,6 @@ def handle_node_message(message):
         print('cant-nodes:{}'.format(len(nodes)))
         write_json("nodes.json", nodes)
 
-    print(is_nodes_done(nodes))
-    # TODO: Death nodes
-    if is_nodes_done(nodes):
-        print("Recibí todos los mensajes")
-        print("Gano: {}".format(get_consensus_winner()))
-        reset_consensus_nodes()
-
 
 def reset_consensus_nodes():
     nodes = read_json("nodes.json")
@@ -121,7 +141,7 @@ def handle_message(message):
 def listener_api_messages():
     with subscriber:
         subscriptions = []
-        for sub in subscriber.list_subscriptions(request={"project": 'projects/' + project_id}):
+        for sub in subscriber.list_subscriptions(request={"project": 'projects/' + settings.project_id}):
             subscriptions.append(sub.name)
 
         if api_topic_subscription_path not in subscriptions:
@@ -147,39 +167,5 @@ def callback(message):
     handle_message(data)
 
 
-if __name__ == "__main__":
-    # node_id = sys.argv[1]
-    winner_1 = ''
-    count = 1
-    size = 0
-
-    # Autenticación
-    credentials_path = "service-account-info.json"
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
-
-    # Estas variables se deben mover a variables de entorno
-    project_id = 'flash-ward-360216'
-    api_topic_subscription_id = 'vocero-sub-' + NODE_ID
-    node_topic_id = 'nodes_info'
-    api_topic_id = 'vocero'
-
-    # Creando el suscriptor
-    subscriber = pubsub_v1.SubscriberClient()
-    api_topic_subscription_path = subscriber.subscription_path(
-        project_id, api_topic_subscription_id)
-
-    # Se inicializa el publisher
-    node_publisher = pubsub_v1.PublisherClient()
-    node_topic_path = node_publisher.topic_path(project_id, node_topic_id)
-    api_publisher = pubsub_v1.PublisherClient()
-    api_topic_path = api_publisher.topic_path(project_id, api_topic_id)
-
-   #future = subscriber.subscribe(api_topic_subscription_path, callback=callback)
-
-   #executor = ThreadPoolExecutor(max_workers=2)
-
-    node_messages_thread = threading.Thread(target=listener_api_messages)
-    node_messages_thread.start()
-
-   # executor.submit(listener_api_messages)
-    # listener_api_messages()
+node_messages_thread = threading.Thread(target=listener_api_messages)
+node_messages_thread.start()
