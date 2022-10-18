6fdaec31ff83e1d7e87c7bb4b3b8d1124d5a8ac3
monotera <nelson.a.mosquera@gmail.com>
2022-10-18 12:06:07 -0500

    Fixes minor issues

--- test/auditUnitTest.py
+++ test/auditUnitTest.py
@@ -49,7 +49,7 @@ def tamper_blockchain():
         {
             "timestamp": 1663608898836871600,
             "last_hash": "0000000000000000000000000000000000000000000000000000000000000000",
-            "hash": "a571e724db5acd737c9e45f798dae743aed2d55484567f1d307521673c5b39ee",
+            "hash": "a571e724db5acd737c9e45f798dae743afd2d55484567f1d307521673c5b39ee",
             "merkle_root": None,
             "number_transactions": 0,
             "transactions_hashes": []
@@ -58,7 +58,7 @@ def tamper_blockchain():
             "timestamp": 1665987251019110142,
             "last_hash": "a571e724ob5acd737c9e45f798dae743aed2d55484567f1d307521673c5b39ee",
             "hash": "1f23d3f698424d4e491cce9ed4f07553f2476b8b5904123214c54be0edaa9bb9",
-            "merkle_root": "fb63851a5a598b86956b3c3fdafc361a8c744f28dd294fa33e79ee99d6ad15eb",
+            "merkle_root": "fb63851f5a598b86956b3c3fdafc361a8c744f28dd294fa33e79ee99d6ad15eb",
             "number_transactions": 3,
             "transactions_hashes": [
                 "a489044d36c5faf1b4bfb8ee43ad4ad3bc94852e75bf5a34260dbf753a759f6e",
@@ -68,9 +68,9 @@ def tamper_blockchain():
         },
         {
             "timestamp": 1665987425619192365,
-            "last_hash": "1f23d3gg98424d4e491cce9ed4f07553f2476b8b5904123214c54be0edaa9bb9",
+            "last_hash": "1f23d3gg98424d4e49fcce9ed4f07553f2476b8b5904123214c54be0edaa9bb9",
             "hash": "4506b659ec6d58794ff5feb1a0c4276ba77228abd300b9c5c73bb0851edd4b13",
-            "merkle_root": "b82d40638c42b0f394692ccc23b99658039f16b7fee1d3468ac40995638ce5b6",
+            "merkle_root": "b82d40638c52b0f394692ccc23b99658039f16b7fee1d3468ac40995638ce5b6",
             "number_transactions": 3,
             "transactions_hashes": [
                 "dfcb15c31e3cbdaf781f142b2f362c8e98eda1085d9e4786fbad8d5cab66a047",
